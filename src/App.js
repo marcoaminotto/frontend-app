b7ac596739c10626cee3bf4406a0d242edf12a4e
Marco Echevestre <marcoaminotto@hotmail.com>
2019-04-15 00:39:17 -0300

    Front-end application

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import Routes from './routes';
+
+const App = () => <Routes />;
+
+
+//Componente stateless, é o mesmo que a função acima. 
+//Para usar o código comentado abaixo, deve-se utilizar ",{ Component }", antes do "from" da primeira linha.
+
+// class App extends Component {
+//   render() {
+//     return <Main />;
+//   }
+// }
+
+export default App;
